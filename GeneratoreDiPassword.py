0c47907109f6d960843546b813e20ddbc70a9bbe
Noah <132379634+noah-yt@users.noreply.github.com>
2024-01-16 19:10:38 +0100

    Update GeneratoreDiPassword.py

--- GeneratoreDiPassword.py
+++ GeneratoreDiPassword.py
@@ -4,8 +4,8 @@ char = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
 passw = ""
 for i in range(int(input("numero caratteri: "))):
     passw += (char[random.randint(0,71)])
-    print(termcolor.colored(passw,"green"))
 
+print(termcolor.colored(passw,"green"))
 
 if input(termcolor.colored("salvare a file? [y/n] ", "red")) == "y":
     with open("password.txt", "w") as f:
