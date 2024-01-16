3a681994f3b4b8c593812cd949fdfc014df1ec2e
Noah <132379634+noah-yt@users.noreply.github.com>
2024-01-16 19:16:59 +0100

    Update GeneratoreDiPassword.py

--- GeneratoreDiPassword.py
+++ GeneratoreDiPassword.py
@@ -2,11 +2,15 @@ import termcolor
 import random
 char = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 passw = ""
-for i in range(int(input("numero caratteri: "))):
-    passw += (char[random.randint(0,71)])
 
-print(termcolor.colored(passw,"green"))
+while True:
+    try:
+        for i in range(int(input("numero caratteri [stop x uscire]: "))):
+            passw += (char[random.randint(0,71)])
+    except Exception:
+        break
+    print(termcolor.colored(passw,"green"))
 
-if input(termcolor.colored("salvare a file? [y/n] ", "red")) == "y":
-    with open("password.txt", "w") as f:
-        f.write(passw)
+    if input(termcolor.colored("salvare a file? [y/n] ", "red")) == "y":
+        with open("password.txt", "w") as f:
+            f.write(passw)
